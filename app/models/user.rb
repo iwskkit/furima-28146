4bb9b009b12c0d28101503f56a1e5dace2652af7
iwskkit <o8o951oo727@docomo.ne.jp>
2020-11-20 12:28:31 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { maximum:50 }
+  validates :name_sei, presence: true
+  validates :name_mei, presence: true
+  validates :name_sei_kana, presence: true
+  validates :name_mei_kana, presence: true
+  validates :birth, presence: true
 end
