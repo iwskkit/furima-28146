a28cbeb111698e79259d36b16633c54efcd8a275
iwskkit <o8o951oo727@docomo.ne.jp>
2020-11-27 15:10:14 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true, length: { maximum:40 }
   validates :name_sei, presence: true, format: { with: honmyou }
   validates :name_mei, presence: true, format: { with: honmyou }
-  validates :name_sei_kana, presence: true { with: katakana }
-  validates :name_mei_kana, presence: true { with: katakana }
+  validates :name_sei_kana, presence: true, format: { with: katakana }
+  validates :name_mei_kana, presence: true, format: { with: katakana }
   validates :birth, presence: true
 end
