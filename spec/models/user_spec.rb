2d52b4c836ccdcd02599dc671cbe07df8bdaa00c
iwskkit <o8o951oo727@docomo.ne.jp>
2020-11-27 12:54:40 +0900

    単体テストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,18 @@ describe User do
       it "passwordが6文字以上であれば登録できる" do
         expect(@user).to be_valid
       end
+      it "name_seiが全角(漢字、カタカナ、ひらがな)であれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "name_meiが全角(漢字、カタカナ、ひらがな)であれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "name_sei_kanaが全角(カタカナ)であれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "name_mei_kanaが全角(カタカナ)であれば登録できる" do
+        expect(@user).to be_valid
+      end
     end
 
     context '新規登録がうまくいかないとき' do
@@ -40,6 +52,11 @@ describe User do
         another_user.valid?
         expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
+      it "emailが@がないと登録できない" do
+        @user.email = "aaaaaaaaa"
+        user.valid?
+        expect(user.errors.full_messages).to include("Email can't include @")
+      end
       it "passwordが空では登録できない" do
         @user.encrypted_password = ""
         user.valid?
@@ -61,15 +78,30 @@ describe User do
         user.valid?
         expect(user.errors.full_messages).to include("Name_sei can't be blank")
       end
+      it "name_seiがアルファベットだと登録できない" do
+        @user.name_sei = "a"
+        user.valid?
+        expect(user.errors.full_messages).to include("Name_sei can't be alphabet")
+      end
       it "name_meiが空だと登録できない" do
         @user.name_mei = ""
         user.valid?
         expect(user.errors.full_messages).to include("Name_mei can't be blank")
       end
+      it "name_meiがアルファベットだと登録できない" do
+        @user.name_mei = "a"
+        user.valid?
+        expect(user.errors.full_messages).to include("Name_mei can't be alphabet")
+      end
       it "name_sei_kanaが空だと登録できない" do
         @user.name_sei_kana = ""
         user.valid?
         expect(user.errors.full_messages).to include("Name_sei_kana can't be blank")
+      end
+      it "name_sei_kanaがだと登録できない" do
+        @user.name_sei = ""
+        user.valid?
+        expect(user.errors.full_messages).to include("Name_sei can't be blank")
       end
       it "name_mei_kanaが空だと登録できない" do
         @user.name_mei_kana = ""
