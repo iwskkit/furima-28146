b7bf12d98131831470fb264e8b156d3b5c532bf3
iwskkit <o8o951oo727@docomo.ne.jp>
2020-11-30 20:58:23 +0900

    itemテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_094418) do
+ActiveRecord::Schema.define(version: 2020_11_30_035437) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "detail", null: false
+    t.integer "price", null: false
+    t.integer "status_id", null: false
+    t.integer "fee_burden_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "handling_time_id", null: false
+    t.integer "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
