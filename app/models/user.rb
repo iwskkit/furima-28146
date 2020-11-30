85140356c5bdc1e0dc43853e3b5beee4012d61ca
iwskkit <o8o951oo727@docomo.ne.jp>
2020-11-30 12:11:38 +0900

    バリデーション の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ApplicationRecord
 
   honmyou = /\A[ぁ-んァ-ン一-龥]/
   katakana = /\A[ァ-ヶー－]+\z/
+  eisuuzi = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
 
   validates :nickname, presence: true, length: { maximum:40 }
   validates :name_sei, presence: true, format: { with: honmyou }
   validates :name_mei, presence: true, format: { with: honmyou }
   validates :name_sei_kana, presence: true, format: { with: katakana }
   validates :name_mei_kana, presence: true, format: { with: katakana }
+  validates :password, presence: true, format: { with: eisuuzi }
   validates :birth, presence: true
+  
 end
