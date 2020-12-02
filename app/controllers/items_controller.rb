adbf9b0c7a9d978145826b37f36d29e536f2efbc
iwskkit <o8o951oo727@docomo.ne.jp>
2020-12-02 11:08:13 +0900

    new,createのルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,14 @@ class ItemsController < ApplicationController
     @items = Item.order("created_at DESC")
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
   private
   def item_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
