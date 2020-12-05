8cfd9bb3da58a3e117bfcf28330c238792d8f0fb
iwskkit <o8o951oo727@docomo.ne.jp>
2020-12-05 21:22:44 +0900

    バリデーションの修正

--- app/models/fee_burden.rb
+++ app/models/fee_burden.rb
@@ -1,4 +1,4 @@
-class Fee_burden < ActiveHash::Base
+class FeeBurden < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '着払い(購入者負担)' },
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :user
-  belongs_to :category
-  belongs_to :fee_burden
-  belongs_to :handling_time
-  belongs_to :prefecture_id
-  belongs_to :statu
+  belongs_to_active_hash :user
+  belongs_to_active_hash  :category
+  belongs_to_active_hash  :fee_burden
+  belongs_to_active_hash  :handling_time
+  belongs_to_active_hash  :prefecture
+  belongs_to_active_hash  :status
 
   has_one_attached :image
 
@@ -13,18 +13,17 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :name, length: { maximum:40 }
-    validates :detail, ength: { maximum:1000 }
+    validates :detail, length: { maximum:1000 }
     validates :price, format: { with: suuzi, message: "半角数字でないと登録できません" }, numericality: { greater_than_or_equal_to: 300, less_than: 9999999 }
-    validates :statu_id
+    validates :status_id
     validates :fee_burden_id
     validates :prefecture_id
     validates :handling_time_id
     validates :category_id
-    validates :content, presence: true, unless: :was_attached?
   end
 
   with_options numericality: { other_than: 1 } do
-    validates :statu_id
+    validates :status_id
     validates :fee_burden_id
     validates :prefecture_id
     validates :handling_time_id
@@ -32,7 +31,7 @@ class Item < ApplicationRecord
   end
 
   def was_attached?
-    self.image.attached?
+    @item.image.attached?
   end
 
 end
