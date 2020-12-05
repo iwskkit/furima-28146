1926686ad4c6331a096be960f1e01b6afdd2e692
iwskkit <o8o951oo727@docomo.ne.jp>
2020-12-05 20:26:09 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,9 +12,9 @@ class Item < ApplicationRecord
   suuzi = /\A[0-9]+\z/
 
   with_options presence: true do
-    validates :name
-    validates :detail
-    validates :price, formet: { with: suuzi, message: "半角数字でないと登録できません" }, numericality: { greater_than_or_equal_to: 300, less_than: 9999999 }
+    validates :name, length: { maximum:40 }
+    validates :detail, ength: { maximum:1000 }
+    validates :price, format: { with: suuzi, message: "半角数字でないと登録できません" }, numericality: { greater_than_or_equal_to: 300, less_than: 9999999 }
     validates :statu_id
     validates :fee_burden_id
     validates :prefecture_id
