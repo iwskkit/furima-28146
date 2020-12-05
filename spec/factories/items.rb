937bffbc83595744107782e21adabe4b595a0879
iwskkit <o8o951oo727@docomo.ne.jp>
2020-12-05 23:52:04 +0900

    テストコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Internet.username}
+    detail { "商品の詳細" }
+    price { "1234" }
+    status_id { "2" }
+    handling_time_id { "2" }
+    prefecture_id { "2" }
+    category_id { "2" }
+    fee_burden_id { "2" }
   end
 end
