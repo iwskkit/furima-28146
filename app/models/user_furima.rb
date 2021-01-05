19b8a1f3cfb94b04cb62210bf16ce60df1850af7
iwskkit <o8o951oo727@docomo.ne.jp>
2021-01-05 21:38:39 +0900

    モデルの作成

--- app/models/user_furima.rb
+++ app/models/user_furima.rb
@@ -1,6 +1,6 @@
 class UserFurima
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :user, :item
+  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :user_id
@@ -15,7 +15,7 @@ class UserFurima
   end
 
   def save
-    user = User.create()
+    user = User.create(item_id: item.id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, user_id: user.id, item_id: item.id)
     PurchasingManagement.create(user_id: user.id, item_id: item.id)
   end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
+describe Address do
+  before do
+    @address = FactoryBot.build(:address)
+  end
 
-RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '住所登録' do
+    context '住所登録がうまくいくとき' do
+      it "全てが存在すれば登録できる" do
+        expect(@address).to be_valid
+      end
+    end
+
+    context '住所登録がうまくいかないとき' do
+      it "postal_codeが空だと登録できない" do
+        @address.postal_code = ""
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it "municipalityが空だと登録できない" do
+        @address.municipality = ""
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it "prefecture_idが空だと登録できない" do
+        @address.prefecture_id = ""
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it "house_numberが空だと登録できない" do
+        @address.house_number = ""
+        @address.valid?
+        expect(@address.errors.full_messages).to include("House number can't be blank")
+      end
+      it "building_nameが空だと登録できない" do
+        @address.building_name = ""
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Building name can't be blank")
+      end
+      it "phone_numberが空だと登録できない" do
+        @address.phone_number = ""
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Phone number can't be blank")
+      end
+    end
+  end
 end
