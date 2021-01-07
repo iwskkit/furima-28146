56f2fb5acd33184ea4522ffdad8213a5734153d2
iwskkit <o8o951oo727@docomo.ne.jp>
2021-01-08 04:10:44 +0900

    テストコードの修正

--- spec/factories/user_furima.rb
+++ spec/factories/user_furima.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
-  factory :address do
+  factory :user_furima do
     postal_code {"123-1232"}
     prefecture_id {"3"}
     municipality {"東京都渋谷区"}
     house_number {"1234"}
     phone_number {"12345678890"}
+    token {"testtest"}
+    user_id {"1"}
+    item_id {"1"}
   end
 end
---
+++ spec/models/user_furima_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+describe UserFurima do
+  before do
+    @user_furima = FactoryBot.build(:user_furima)
+  end
+
+  describe '購入' do
+    context '購入がうまくいくとき' do
+      it "postal_code,municipality,prefecture_id,house_number,phone_numbe,user_id,item_idrが存在すれば登録できる" do
+        expect(@user_furima).to be_valid
+      end
+    end
+
+    context '購入がうまくいかないとき' do
+      it "postal_codeが空だと登録できない" do
+        @user_furima.postal_code = ""
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it "postal_codeが数字以外だと登録できない" do
+        @user_furima.postal_code = "asd-asdf"
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("Postal code is invalid")
+      end
+      it "postal_codeに-がないと登録できない" do
+        @user_furima.postal_code = "1234567"
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("Postal code is invalid")
+      end
+      it "municipalityが空だと登録できない" do
+        @user_furima.municipality = ""
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it "prefecture_idが空だと登録できない" do
+        @user_furima.prefecture_id = ""
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it "prefecture_idが1だと登録できない" do
+        @user_furima.prefecture_id = "1"
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it "house_numberが空だと登録できない" do
+        @user_furima.house_number = ""
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("House number can't be blank")
+      end
+      it "phone_numberが空だと登録できない" do
+        @user_furima.phone_number = ""
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it "phone_numberが数字以外だと登録できない" do
+        @user_furima.phone_number = "asdqwerr"
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("Phone number is invalid")
+      end
+      it "phone_numberが11文字以上だと登録できない" do
+        @user_furima.phone_number = "123456789012"
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
+      end
+      it "user_idが空だと登録できない" do
+        @user_furima.user_id = ""
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("User can't be blank")
+      end
+      it "item_idが空だと登録できない" do
+        @user_furima.item_id = ""
+        @user_furima.valid?
+        expect(@user_furima.errors.full_messages).to include("Item can't be blank")
+      end
+    end
+  end
+end
