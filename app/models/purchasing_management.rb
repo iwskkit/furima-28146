ced299fecdaa33b3e4c6170c95b3e1ff57b1e3aa
iwskkit <o8o951oo727@docomo.ne.jp>
2021-01-08 04:11:03 +0900

    アソシエーションの設定

--- app/models/purchasing_management.rb
+++ app/models/purchasing_management.rb
@@ -1,5 +1,10 @@
 class PurchasingManagement < ApplicationRecord
   attr_accessor :token
+
+  belongs_to :user
+  belongs_to :item
+  has_one :address
+
   with_options presence: true do
     validates :user_id
     validates :item_id
