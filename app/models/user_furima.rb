e887276dfa551b4fa296cf10fd641a0dfb657193
iwskkit <o8o951oo727@docomo.ne.jp>
2021-01-07 20:45:05 +0900

    form オブジェクトの修正

--- app/models/user_furima.rb
+++ app/models/user_furima.rb
@@ -1,6 +1,6 @@
 class UserFurima
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :user_id, :item_id, :token, :purchasing_management_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :user_id
@@ -9,16 +9,13 @@ class UserFurima
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipality
     validates :house_number
-    validates :building_name
     validates :phone_number, format: { with: /\A[0-9]+\z/ }, length: { maximum:11 }
-    validates :purchasing_management_id
     validates :token
   end
 
   def save
-    user = User.create(item_id: item.id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, user_id: user.id, item_id: item.id)
-    PurchasingManagement.create(user_id: user.id, item_id: item.id)
+    purchasing_management = PurchasingManagement.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, purchasing_management_id: purchasing_management.id)
   end
 
 end
