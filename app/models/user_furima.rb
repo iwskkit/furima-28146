7b176457dbcfbf9a0341bc171774e0f5db4f8013
iwskkit <o8o951oo727@docomo.ne.jp>
2021-01-07 21:10:55 +0900

    formオブジェクトの修正

--- app/models/user_furima.rb
+++ app/models/user_furima.rb
@@ -14,8 +14,8 @@ class UserFurima
   end
 
   def save
-    purchasing_management = PurchasingManagement.create!(user_id: user_id, item_id: item_id)
-    Address.create!(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, purchasing_management_id: purchasing_management.id)
+    purchasing_management = PurchasingManagement.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, purchasing_management_id: purchasing_management.id)
   end
 
 end
