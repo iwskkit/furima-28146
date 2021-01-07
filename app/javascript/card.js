5e4aa55a22cd7d80f77da0e1f8a3239ecb7eec7b
iwskkit <o8o951oo727@docomo.ne.jp>
2021-01-07 12:56:50 +0900

    Payjpの修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -9,10 +9,10 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      cvc: formData.get("order[cvc]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
+      number: formData.get("user_furima[number]"),
+      cvc: formData.get("user_furima[cvc]"),
+      exp_month: formData.get("user_furima[exp_month]"),
+      exp_year: `20${formData.get("user_furima[exp_year]")}`,
     };
 
     Payjp.createToken(card, (status, response) => {
@@ -24,10 +24,10 @@ const pay = () => {
         debugger;
       }
 
-      document.getElementById("order_number").removeAttribute("name");
-      document.getElementById("order_cvc").removeAttribute("name");
-      document.getElementById("order_exp_month").removeAttribute("name");
-      document.getElementById("order_exp_year").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
@@ -35,3 +35,4 @@ const pay = () => {
 };
 
 window.addEventListener("load", pay);
+
